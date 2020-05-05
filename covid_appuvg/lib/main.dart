965e742d915c18887a43f83d0bf87dddc97931ab
André Rodríguez <andre.rodriguezgt@yahoo.com>
2020-05-05 16:17:59 -0600

    Interfaz login finalizado

--- covid_appuvg/lib/main.dart
+++ covid_appuvg/lib/main.dart
@@ -1,111 +1,201 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final _biggerFont = const TextStyle(
+    fontSize: 28.0,
+    color: Colors.white,
+    fontFamily: 'SeoulNamsamCondensed'
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CovidApp UVG',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Color.fromRGBO(7, 139, 69, 1)
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        backgroundColor: Color.fromRGBO(59, 58, 59, 1),
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(70.0),
+          child: AppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: Color.fromRGBO(59, 58, 59, 1),
+            flexibleSpace: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'assets/uvglogo.png',
+                  fit: BoxFit.contain,
+                  height: 90,
+                ),
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      'INICIAR SESIÓN',
+                      style: _biggerFont,
+                    ),
+                  )
+                )
+              ],
+            ),
+          ),
+        ),
+        body: Column(
+          children: <Widget>[
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: Container(
+                height: 20,
+                color: Color.fromRGBO(7, 139, 69, 1),
+              ),
+            ),
+            Login(),
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: Container(
+                height: 40,
+                color: Color.fromRGBO(59, 58, 59, 1),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Login extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    return LoginState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class LoginState extends State<Login> {
+  final _biggerFont = const TextStyle(
+    fontSize: 30.0,
+    color: Color.fromRGBO(59, 58, 59, 1),
+    fontFamily: 'SeoulNamsamCondensed'
+  );
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final _smallerFont = const TextStyle(
+    fontSize: 15.0,
+    color: Colors.white,
+    fontFamily: 'SeoulNamsamCondensed'
+  );
+  
+  final mController = TextEditingController();
+  final mController2 = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    mController.dispose();
+    mController2.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    return Expanded(
+      child: FractionallySizedBox(
+        widthFactor: 1,
+        child: Container(
+          color: Colors.white,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+              campo('Usuario', mController),
+              campo('Contraseña', mController2),
+              Center(
+                child: Container(
+                  padding: EdgeInsets.only(
+                    top: 20
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.transparent,
+                  ),
+                  child: Theme(
+                    data: Theme.of(context).copyWith(splashColor: Colors.transparent), 
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.green,
+                      ),
+                      child: FractionallySizedBox(
+                        widthFactor: 0.75,
+                        child: FlatButton(
+                          onPressed: null,
+                          color: Colors.green,
+                          child: Text(
+                            'INICIAR SESIÓN',
+                            style: _smallerFont,
+                          ),
+                        ),
+                      ),
+                      height: 40,                          
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Widget campo(String labelMessage, TextEditingController myController) {
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: Center(
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 20,
+            bottom: 20
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                labelMessage,
+                style: _biggerFont,
+              ),
+              Theme(
+                data: Theme.of(context).copyWith(splashColor: Colors.transparent), 
+                child: FractionallySizedBox(
+                  widthFactor: 0.75,
+                  child: Container(
+                    height: 30,
+                    child: TextField(
+                      controller: myController,
+                      autofocus: false,
+                      style: _smallerFont,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color.fromRGBO(231, 231, 231, 1),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
+                          borderRadius: BorderRadius.circular(10)
+                        )
+                      ),
+                    ),
+                  ),
+                )
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
 }
